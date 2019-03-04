4b1363ebbc13af94705fea5e6b838722c9cac81d
unknown <levlevit@gmail.com>
2019-03-04 09:43:52 -0500

    Code Challanges

--- Lev_Levit_CodingChallange2_Week1.js
+++ Lev_Levit_CodingChallange2_Week1.js
@@ -1,7 +1,7 @@
 let challenge = {};
 
 challenge.fibbonaci = function(n){
-    if (n==1 || n==2)
+    if (n<=2)
         return 1;
     else
         return this.fibbonaci(n-1) + this.fibbonaci(n-2);

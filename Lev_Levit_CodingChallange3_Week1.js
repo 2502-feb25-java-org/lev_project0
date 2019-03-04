bbab7ae728cd791c9da5b5509e1b5c8bc50c57a1
unknown <levlevit@gmail.com>
2019-03-04 09:58:44 -0500

    Code Challanges

---
+++ Lev_Levit_CodingChallange3_Week1.js
@@ -0,0 +1,13 @@
+let challenge = {};
+
+challenge.sort = function (array) {
+    for (let i = 0; i < array.length; i++) {
+        for (j = 0; j < array.length - i - 1; j++)
+            if (array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+    }
+    return array;
+}

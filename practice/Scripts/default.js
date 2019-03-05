01ba6c2d27e0f967cf6fec487c81a8fc94357e88
unknown <levlevit@gmail.com>
2019-03-04 21:42:11 -0500

    Project0 Update

--- practice/Scripts/default.js
+++ practice/Scripts/default.js
@@ -1,44 +1,44 @@
-function storeValue() {
-    let elementId = event.target.id;
-    let element = document.getElementById(elementId);
-    if (element.type == "radio")
-        localStorage.setItem(element.name, elementId);
-    else
-        localStorage.setItem(elementId, element.value);
-}
-
-function loadValue(elementId) {
-    let element = document.getElementById(elementId);
-    if (element.type == "radio")
-        loadRadios(element.name);
-    else
-        element.value = localStorage.getItem(elementId);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    reloadValues();
+    let inputTags = document.getElementsByTagName("input");
+    for (let index = 0; index < inputTags.length; index++) {
+        inputTags[index].addEventListener("change", () => {
+            storeValue(inputTags[index]);
+        });
+    }
+});
 
 function reloadValues() {
-    loadValue("first_name");
-    loadValue("last_name");
-    loadValue("male");
-    loadValue("female");
-    loadValue("other");
-    loadValue("age");
-    loadValue("phone");
-    loadValue("email");
+    let inputTags = document.getElementsByTagName("input");
+    for (let index = 0; index < inputTags.length; index++) {
+        loadValue(inputTags[index]);
+    };
+}
+
+function storeValue(element) {
+    if (element.type == "radio")
+        localStorage.setItem(element.name, element.id);
+    else
+        localStorage.setItem(element.id, element.value);
+}
+
+function loadValue(element) {
+    if (element.type == "submit")
+        return;
+    else if (element.type == "radio")
+        loadRadios(element.name);
+    else
+        element.value = localStorage.getItem(element.id);
 }
 
 function loadRadios(elementName) {
-    debugger;
     let radios = document.getElementsByName(elementName);
-    //console.log(radios);
     let storedValue = localStorage.getItem(elementName);
     for (let index = 0; index < radios.length; index++) {
-        if (radios[index].id == storedValue) {
-            radios[index].value = "On";
+        if (radios[index].id == storedValue)
+            radios[index].checked = true;
 
-        }
-        else {
-            radios[index].value = "Off";
-            alert(radios[index].value);
-        }
+        else
+            radios[index].checked = false;
     }
 }
